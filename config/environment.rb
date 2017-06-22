8d5e145b37ae0b16dd67d1c96fab9a34c179dbb2
Danniel Albuquerque <danniel.master@gmail.com>
2017-06-22 15:14:35 -0300

    alterando forma padrão do rails de exibição de erros nos formulários

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  "<div class='has-error'>#{html_tag}</div>".html_safe
+end
