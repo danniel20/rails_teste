0e58fb3678360087ded6972b4067c7a4af406b33
Danniel Albuquerque <danniel.master@gmail.com>
2017-07-27 16:17:50 -0300

    alteração helper rails 5.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,7 @@ module ApplicationHelper
 
 	def error_tag(model, attribute)
 		if model.errors.has_key?(attribute)
-			#content_tag(:div, model.errors[attribute].first, :class => 'text-danger')
-			content_tag(:div, content_tag(:span, "",:class => 'glyphicon glyphicon-remove', :'aria-hidden' => "true"))
+			tag.div(tag.span("", :class => 'glyphicon glyphicon-remove', :'aria-hidden' => "true"))
 		end
 	end
 
